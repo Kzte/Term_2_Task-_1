271f08ee6daf7cb8518e0ef91cbb30957e8b7920
Kzte <231078@virtualwindow.co.za>
2023-05-06 10:43:35 +0200

    Creating html and java script
    Creating and implementing  html and java script

---
+++ js/index.js
@@ -0,0 +1,10 @@
+console.log("Hello World")
+
+var firstName = "Kate-Lynn"
+let surname = "Guy"
+let age = 19
+const subject = "Development"
+
+let message = "Hello ," + " my name is " + firstName +  "  " + surname + " , I am  " + age + " years old, and I am studying " +  subject  
+
+console.log(message)
